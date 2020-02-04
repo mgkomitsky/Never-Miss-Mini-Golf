c3c816e2dcfe4cabe2284499641ced32a3e2702d
mgkomitsky <40476432+mgkomitsky@users.noreply.github.com>
2020-02-04 10:45:49 -0600

    Update Motor Controller.ino

--- Motor Controller.ino
+++ Motor Controller.ino
@@ -1,4 +1,4 @@
-#include <Stepper.h>
+
 
 
 // Example 4 - Receive a number as text and convert it to an int
@@ -9,13 +9,13 @@ char receivedChars[numChars];   // an array to store the received data
 boolean newData = false;
 
 int projected_target_pixels = 0;            
-int current_position_pixels = 0;
-int pixels_per_step = 1;
+int current_position_pixels = 50;
+int pixels_per_step = 2;
 int number_of_pixels = 0;
 int number_of_steps = 0;
 int target_zone_length_in_steps = 10;
 int small_boundary = 0;
-int big_boundary = 10;
+int big_boundary = 3000;
 
 
 int left_limit_switch = 2;
@@ -31,14 +31,17 @@ void setup() {
     
     Serial.begin(9600);  
     delay(1000);
-    Serial.println("<Arduino is ready>");
+    //Serial.println("<Arduino is ready>");
     pinMode(LED_BUILTIN,OUTPUT);
     pinMode(9,OUTPUT); //Direction
     pinMode(10, OUTPUT); //Enable
-    pinMode(2,INPUT); //Right Limit
-    pinMode(3,INPUT); //Left Limit
-    attachInterrupt(digitalPinToInterrupt(left_limit_switch), homeDevice_towards_right, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(right_limit_switch), homeDevice_towards_left, CHANGE);
+    pinMode(7, OUTPUT); //Pulse
+    //pinMode(2,INPUT); //Right Limit
+    //pinMode(3,INPUT); //Left Limit
+    //attachInterrupt(digitalPinToInterrupt(left_limit_switch), homeDevice_towards_right, CHANGE);
+    //attachInterrupt(digitalPinToInterrupt(right_limit_switch), homeDevice_towards_left, CHANGE);
+
+    //digitalWrite(motor_pulse_pin,LOW);
     
     
     //homeDevice_towards_left();
@@ -49,6 +52,8 @@ void setup() {
 
 
 void loop() {
+  
+  
     recvWithEndMarker();
     showNewNumber();
 }
@@ -121,36 +126,38 @@ void move_motor(int number_of_steps)
 if(number_of_steps > 0)
   {
 
-        digitalWrite(enable_pin,HIGH);//enable
+        digitalWrite(enable_pin,LOW);//enable
         digitalWrite(direction_pin,HIGH);//set direction
 
       for (int i = 0; i <= number_of_steps; i++)
 
         {
           digitalWrite(motor_pulse_pin,HIGH);
-          delay(10);
+          delay(1);
           digitalWrite(motor_pulse_pin,LOW);
+          
         }
    
-        digitalWrite(enable_pin,LOW);//disable  
+        digitalWrite(enable_pin,HIGH);//disable  
   } 
 
   else 
   
   {
-        digitalWrite(10,HIGH);//enable
+        digitalWrite(10,LOW);//enable
         digitalWrite(direction_pin,LOW);//set direction
    
 
-      for (int i = 0; i <= number_of_steps; i--)
+      for (int i = 0; i >= number_of_steps; i--)
 
         {
           digitalWrite(motor_pulse_pin,HIGH);
-          delay(10);
+          delay(1);
           digitalWrite(motor_pulse_pin,LOW);
+         
         }
       
-        digitalWrite(10,LOW);//disable  
+        digitalWrite(10,HIGH);//disable  
   } 
   
 }
@@ -177,7 +184,7 @@ void showNewNumber() {
     {
         projected_target_pixels = 0;            
         projected_target_pixels = atoi(receivedChars);   
-        if (abs(projected_target_pixels)>100)
+        if (abs(projected_target_pixels)>2000)
         {
          projected_target_pixels = 0; 
          }
